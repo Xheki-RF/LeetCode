cbe69994c95d899b7f074a59d58558c644a4948c
XHEKI <pashokts@mail.ru>
2025-03-06 19:41:41 +0300

    Added solution for 2nd prolblem

--- 2.py
+++ 2.py
@@ -1,12 +1,50 @@
-from LLlib import ListNode
+from typing import Optional
 
-# class Solution(object):
-#     def addTwoNumbers(self, l1, l2):
-#         for i in l1:
-#             print(i)
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-# print(Solution().addTwoNumbers([2,4,3], [5,6,4]))
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        lst1, lst2 = 0, 0
+        g = 0
+        while l1 != None:
+            lst1 += l1.val * 10 ** g
+            g += 1
+            l1 = l1.next
+        g = 0
+        while l2 != None:
+            lst2 += l2.val * 10 ** g
+            g += 1
+            l2 = l2.next
 
-x = ListNode([2, 3, 4])
-f = x.val
-print(f)
+        line = tuple((int(x) for x in str(lst1 + lst2)))
+
+        res = ListNode(line[0])
+
+        for i in range(len(line)):
+            if i == 0:
+                res = ListNode(line[i])
+            else:
+                tmp = ListNode(line[i])
+                tmp.next = res
+                res = tmp
+
+        return res
+
+# List 1
+a_1 = ListNode(2)
+a_2 = ListNode(4)
+a_3 = ListNode(3)
+a_1.next = a_2
+a_2.next = a_3
+
+# List 2
+b_1 = ListNode(5)
+b_2 = ListNode(6)
+b_3 = ListNode(4)
+b_1.next = b_2
+b_2.next = b_3
+
+Solution().addTwoNumbers(a_1, b_1)
